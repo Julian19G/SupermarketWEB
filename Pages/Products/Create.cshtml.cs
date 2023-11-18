7d770fd2999dbfea3fee80405df79a4fa03cd505
Julian <julian19guerra@gmail.com>
2023-11-18 12:52:51 -0500

    Create products

---
+++ Pages/Products/Create.cshtml.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.Products
+{
+    public class CreateModel : PageModel
+    {
+        private readonly SupermarketContext _context;
+
+        public CreateModel(SupermarketContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Product Product { get; set; } = default!;
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid || _context.Products == null || Product == null)
+            {
+                return Page();
+            }
+            try
+            {
+
+
+                _context.Products.Add(Product);
+                await _context.SaveChangesAsync();
+
+                return RedirectToPage("./Index");
+            }
+
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error al intentar guardar el producto. Por favor, inténtalo de nuevo.");
+                return Page();
+            }
+        }
+    }
+}

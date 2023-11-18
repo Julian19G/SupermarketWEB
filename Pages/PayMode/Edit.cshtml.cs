9cd80f57fb10b6de9ba4885bfab8a63b002dcc06
Julian <julian19guerra@gmail.com>
2023-11-18 13:39:07 -0500

    Paymodes

--- Pages/PayMode/Edit.cshtml.cs
+++ Pages/PayMode/Edit.cshtml.cs
@@ -1,55 +1,68 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using SupermarketWEB.Data;
 using SupermarketWEB.Models;
 
 namespace SupermarketWEB.Pages.PayMode
 {
-    public class DeleteModel : PageModel
+    public class EditModel : PageModel
     {
         private readonly SupermarketContext _context;
-
-        public DeleteModel(SupermarketContext context)
+        public EditModel(SupermarketContext context)
         {
             _context = context;
         }
 
         [BindProperty]
-        public PayModes PayModes { get; set; } = default!;
 
+        public PayModes PayModes { get; set; } = default!;
         public async Task<IActionResult> OnGetAsync(int? id)
         {
             if (id == null || _context.PayMod == null)
             {
                 return NotFound();
             }
-
             var paymodes = await _context.PayMod.FirstOrDefaultAsync(m => m.Id == id);
-
             if (paymodes == null)
             {
                 return NotFound();
             }
-            else
-            {
-                PayModes = paymodes;
-            }
+            PayModes = paymodes;
             return Page();
         }
-        public async Task<IActionResult> OnPostAsync(int? id)
+
+
+        public async Task<IActionResult> OnPostAsync()
         {
-            if (id == null || _context.PayMod == null)
+            if (!ModelState.IsValid)
             {
-                return NotFound();
+                return Page();
             }
-            var paymodes = await _context.PayMod.FindAsync(id);
-            if (paymodes != null)
+
+            _context.Attach(PayModes).State = EntityState.Modified;
+
+            try
             {
-                PayModes = paymodes;
-                _context.PayMod.Remove(PayModes);
                 await _context.SaveChangesAsync();
             }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PayModesExists(PayModes.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
             return RedirectToPage("./Index");
+        }
+        private bool PayModesExists(int id)
+        {
+            return (_context.PayMod?.Any(e => e.Id == id)).GetValueOrDefault();
         }
     }
 }
